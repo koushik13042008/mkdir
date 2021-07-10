694c36d8510d178a169de7dc49e52024a4ac6093
koushik13042008 <gkoushik1304@gmail.com>
2021-07-10 21:30:51 +0530

    created balls

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
 const Constraint = Matter.Constraint;
-var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject
+var bob1,bob2,bob3, bob4,bob5, roofObject
 var rope1,rope2,rope3, rope4,rope5;
 var world;
 
@@ -21,11 +21,16 @@ function setup() {
 	roofObject=new roof(400,250,230,20);
 	bob1 = new bob(320,575,40)
 	bob2 = new bob(360,575,40)
+	bob3 = new bob(400,575,40)
+	bob4 = new bob(440,575,40)
+	bob5 = new bob(480,575,40)
 	
 	
 	rope1=new rope(bob1.body,roofObject.body,-80, 0)
 	rope2=new rope(bob2.body,roofObject.body,-40, 0)
-	
+	rope3=new rope(bob3.body,roofObject.body,0, 0)
+	rope4=new rope(bob4.body,roofObject.body,40, 0)
+	rope5=new rope(bob5.body,roofObject.body,80, 0)
 	
 	Engine.run(engine);
 	
@@ -33,6 +38,14 @@ function setup() {
 }
 
 
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+	
+		 Matter.Body.applyForce(bob1.body,bob1.body.position,
+								{x:-50,y:-45});
+	
+		  }
+	}
 function draw() {
   rectMode(CENTER);
   background(230);
@@ -40,16 +53,17 @@ function draw() {
 
   rope1.display();
   rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
 
   bob1.display();
   bob2.display();
   
+  bob3.display();
+  bob4.display();
+  bob5.display();
+
   
  
 }
-
-
-
-
-
-
